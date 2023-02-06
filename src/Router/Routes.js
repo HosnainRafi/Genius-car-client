4aad0cfd6f6381475ea8f0e2c4d5c50931ed4d2d
Your <you@example.com>
2023-02-06 09:16:53 +0600

    first commit

---
+++ src/Router/Routes.js
@@ -0,0 +1,24 @@
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element:<Main></Main> ,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/home',
+                element:<Home></Home>
+            },
+        ]
+    }
+]);
+
+export default router;
